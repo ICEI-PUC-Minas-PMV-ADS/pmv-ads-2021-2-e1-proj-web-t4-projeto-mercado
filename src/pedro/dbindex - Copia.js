e0a5d5e58715089815f02baa61a46142acffd248
XIMA87 <91227083+XIMA87@users.noreply.github.com>
2021-11-28 10:22:24 -0300

    Add files via upload

---
+++ src/pedro/dbindex - Copia.js
@@ -0,0 +1,53 @@
+var dataBase = {
+    dados: [
+        {
+            titulo: 'Refrigerante de Cola',
+            descricao: 'Refrigerante gaseificado',
+            imagem: 'http://lorempixel.com/200/120/food/1',
+            categoria: 'bebidas',
+            preco: 8.00
+        }, {
+            titulo: 'Tempero pronto',
+            descricao: 'Tempero pronto de sal e alho',
+            imagem: 'http://lorempixel.com/200/120/food/2',
+            categoria: 'temperos',
+            preco: 30.00
+        }, {
+            titulo: 'Contra Filé',
+            descricao: '1kg de Contra Filé',
+            imagem: 'http://lorempixel.com/200/120/food/3',
+            categoria: 'carnes',
+            preco: 80.00
+        }, {
+            titulo: 'Pizza congelada',
+            descricao: '1 caixa de pizza congelada pronta para o forno',
+            imagem: 'http://lorempixel.com/200/120/food/4',
+            categoria: 'comidas',
+            preco: 20.00
+        }, {
+            titulo: 'Doce de Leite Viçosa',
+            descricao: '1 lata de doce de leite',
+            imagem: 'http://lorempixel.com/200/120/food/5',
+            categoria: 'sobremesas',
+            preco: 30.00
+        }, {
+            titulo: 'Guaraná',
+            descricao: 'Refrigerante gaseificado',
+            imagem: 'http://lorempixel.com/200/120/food/3',
+            categoria: 'bebidas',
+            preco: 7.00
+        }, {
+            titulo: 'Alface',
+            descricao: 'Alface organica',
+            imagem: 'http://lorempixel.com/200/120/food/4',
+            categoria: 'comidas',
+            preco: 2.00
+        }, {
+            titulo: 'Sal grosso',
+            descricao: 'Saquinho de sal grosso.',
+            imagem: 'http://lorempixel.com/200/120/food/5',
+            categoria: 'temperos',
+            preco: 3.00
+        }
+    ]
+}
