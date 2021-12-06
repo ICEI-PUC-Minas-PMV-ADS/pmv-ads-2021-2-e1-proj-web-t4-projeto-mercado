9b7614a05d19f49dcfb05bcc29932ae11b67c42d
XIMA87 <91227083+XIMA87@users.noreply.github.com>
2021-12-06 10:06:48 -0300

    Add files via upload

--- src/pedro/script.js
+++ src/pedro/script.js
@@ -3,49 +3,49 @@ var dataBasePromo = {
         {
             titulo: 'Refrigerante de Cola',
             descricao: '50% de desconto no Edmundo Taioba',
-            imagem: 'http://lorempixel.com/200/200/food/1',
+            imagem: 'http://via.placeholder.com/200x200',
             categoria: 'bebidas',
             preco: 4.00
         }, {
             titulo: 'Tempero pronto',
             descricao: '20% de desconto no Zé das Couves',
-            imagem: 'http://lorempixel.com/200/200/food/2',
+            imagem: 'http://via.placeholder.com/200x200',
             categoria: 'temperos',
             preco: 24.00
         }, {
             titulo: 'Contra Filé',
             descricao: '10% de desconto no Zé das Couves',
-            imagem: 'http://lorempixel.com/200/200/food/3',
+            imagem: 'http://via.placeholder.com/200x200',
             categoria: 'carnes',
             preco: 72.00
         }, {
             titulo: 'Pizza congelada',
             descricao: '10% de desconto no Edmundo Taioba',
-            imagem: 'http://lorempixel.com/200/200/food/4',
+            imagem: 'http://via.placeholder.com/200x200',
             categoria: 'comidas',
             preco: 18.00
         }, {
             titulo: 'Doce de Leite Viçosa',
             descricao: '50% de desconto no Zé das Couves',
-            imagem: 'http://lorempixel.com/200/200/food/5',
+            imagem: 'http://via.placeholder.com/200x200',
             categoria: 'sobremesas',
             preco: 15.00
         }, {
             titulo: 'Guaraná',
             descricao: '50% de desconto no Edmundo Taioba',
-            imagem: 'http://lorempixel.com/200/200/food/3',
+            imagem: 'http://via.placeholder.com/200x200',
             categoria: 'bebidas',
             preco: 3.50
         }, {
             titulo: 'Alface',
             descricao: 'Compre 1 e leve 2 no Zé das Couves',
-            imagem: 'http://lorempixel.com/200/200/food/4',
+            imagem: 'http://via.placeholder.com/200x200',
             categoria: 'comidas',
             preco: 2.00
         }, {
             titulo: 'Sal grosso',
             descricao: '33% de desconto no Zé das Couves',
-            imagem: 'http://lorempixel.com/200/200/food/5',
+            imagem: 'http://via.placeholder.com/200x200',
             categoria: 'temperos',
             preco: 2.00
         }
@@ -57,49 +57,49 @@ var dataBaseProd = {
         {
             titulo: 'Refrigerante de Cola',
             descricao: 'Refrigerante gaseificado de cola',
-            imagem: 'http://lorempixel.com/200/200/food/1',
+            imagem: 'http://via.placeholder.com/200x200',
             categoria: 'bebidas',
             preco: 8.00
         }, {
             titulo: 'Tempero pronto',
             descricao: 'Tempero pronto de sal e alho',
-            imagem: 'http://lorempixel.com/200/200/food/2',
+            imagem: 'http://via.placeholder.com/200x200',
             categoria: 'temperos',
             preco: 30.00
         }, {
             titulo: 'Contra Filé',
             descricao: '1kg de Contra Filé',
-            imagem: 'http://lorempixel.com/200/200/food/3',
+            imagem: 'http://via.placeholder.com/200x200',
             categoria: 'carnes',
             preco: 80.00
         }, {
             titulo: 'Pizza congelada',
             descricao: '1 caixa de pizza congelada',
-            imagem: 'http://lorempixel.com/200/200/food/4',
+            imagem: 'http://via.placeholder.com/200x200',
             categoria: 'comidas',
             preco: 20.00
         }, {
             titulo: 'Doce de Leite Viçosa',
             descricao: '1 lata de doce de leite',
-            imagem: 'http://lorempixel.com/200/200/food/5',
+            imagem: 'http://via.placeholder.com/200x200',
             categoria: 'sobremesas',
             preco: 30.00
         }, {
             titulo: 'Guaraná',
             descricao: 'Refrigerante gaseificado',
-            imagem: 'http://lorempixel.com/200/200/food/3',
+            imagem: 'http://via.placeholder.com/200x200',
             categoria: 'bebidas',
             preco: 7.00
         }, {
             titulo: 'Alface',
             descricao: 'Alface organica',
-            imagem: 'http://lorempixel.com/200/200/food/4',
+            imagem: 'http://via.placeholder.com/200x200',
             categoria: 'comidas',
             preco: 2.00
         }, {
             titulo: 'Sal grosso',
             descricao: 'Saquinho de sal grosso',
-            imagem: 'http://lorempixel.com/200/200/food/5',
+            imagem: 'http://via.placeholder.com/200x200',
             categoria: 'temperos',
             preco: 3.00
         }
